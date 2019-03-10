c48f2efeaef2575a97ce66860102c1b538d88cbb
Hui An Yang <hecatie36@hotmail.com>
2019-03-10 17:21:05 +0000

    mid-charater take testing and return t refactored

--- src/midCharacter.js
+++ src/midCharacter.js
@@ -2,7 +2,7 @@ function midCharacter (string) {
     if(string.length % 2 === 0){
         return string[(string.length/2-1)] + string[(string.length/2)]
     }else if(string.length % 2 !== 0){
-        return 't'
+        return string[Math.floor(string.length/2)]
     }
 }
 
